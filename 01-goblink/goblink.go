1c5827a0252b63ae0bc5471d675b9ee8e6bdb181
Colin <colinnlsn@gmail.com>
2017-10-20 14:35:34 -0500

    Fixed build error and added support to respect user input on the interval

--- 01-goblink/goblink.go
+++ 01-goblink/goblink.go
@@ -8,14 +8,14 @@ import "strconv"
 //import "github.com/stianeikeland/go-rpio"
 
 func main() {
-    fmt.Println("Please enter time interval: ")
+    fmt.Println("Please enter time interval in seconds: ")
     text := ""
     fmt.Scanln(&text)
     seconds, parseErr := strconv.Atoi(text)
 
     for parseErr != nil {
-        fmt.Println("Sorry we didnt recognize \"" + text + "\" as a number. Please try again")
-        fmt.Println("Please enter time interval: ")
+        fmt.Println("Sorry we didnt recognize \"" + text + "\" as an integer. Please try again")
+        fmt.Println("Please enter time interval in seconds: ")
         fmt.Scanln(text)
         seconds, parseErr = strconv.Atoi(text)
     }
@@ -30,7 +30,7 @@ func main() {
     fmt.Println("Exiting")
 }
 
-func pinWorker(seconds float64, done chan<- bool) {
+func pinWorker(seconds int, done chan<- bool) {
     fmt.Println("Starting pinWorker with interval " + strconv.Itoa(seconds))
 
     //err := rpio.Open()
@@ -41,7 +41,7 @@ func pinWorker(seconds float64, done chan<- bool) {
     //defer rpio.Close()
 
     //pin := rpio.Pin(10)
-    ticker := time.NewTicker(time.Second * seconds)
+    ticker := time.NewTicker(time.Second * time.Duration(seconds))
     for range ticker.C {
         //Do some work with the pins???
         fmt.Println("Hey there")
